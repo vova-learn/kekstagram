ef561dbad083dfa2b68a4a157ecf2c4007ee9792
Vladimir <shopme13@yandex.ru>
2020-02-09 10:14:31 +0300

    Переписал код с учетом комментариев

--- js/main.js
+++ js/main.js
@@ -1,101 +1,108 @@
 'use strict';
 
 (function () {
+  var AMOUNT_PHOTOS = 25;
+  var PHOTO_DESCRIPTIONS = [
+    'Кажется, что-то пролетело. Это мой отпуск.',
+    'По догоре на пляж!',
+    'Наша первая встреча с дикими животными.',
+    'Здоровое питание - прекрасное самочувствие!'
+  ];
+  var PHOTO_MIN_LIKE = 15;
+  var PHOTO_MAX_LIKE = 200;
+  var COMMENT_MIN_AVATAR_URL = 1;
+  var COMMENT_MAX_AVATAR_URL = 6;
+  var COMMENT_NAMES = [
+    'Коко',
+    'Лулу',
+    'Гучи',
+    'Кузя',
+    'Пушок'
+  ];
+  var COMMENT_MESSAGES = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ];
 
-  var countCreateImageElements = 25;
-  var minLikes = 15;
-  var maxLikes = 200;
-  var minCommentsAvatar = 1;
-  var maxCommentsAvatar = 6;
+  var templatePost = document.querySelector('#picture')
+    .content
+    .querySelector('.picture');
 
-  var imageDescriptions = ['Сколько слов должно быть в заголовке', 'Классное фото без фотошопа? Я научу!', 'Беги, Форест!', 'Только одна из пяти женщин помнит об…', 'Срочно удаляйте свой браузер!'];
-  var commentsMessages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-  var commentsNames = ['@leomesssi', '@tailorswift', '@miesthecat', '@kendalljennner', '@kevinhartNoreal'];
+  var similarListPosts = document.querySelector('.pictures');
 
-  // создаём массив для выбора случайного числа
-  var createArray = function (counter) {
-    var arr = [];
-    for (var i = 1; i <= counter; i++) {
-      arr.push(i);
-    }
-    return arr;
+  var getRandomElement = function (array) {
+    return array[Math.floor(Math.random() * array.length)];
   };
 
-  var srcImgs = createArray(countCreateImageElements);
-
-  // функция min max случайного числа
   var getRandomNumber = function (min, max) {
     return Math.floor((Math.random() * (max - min)) + min);
   };
 
-  // функция случайного числа из массива
-  var getRandomIndex = function (arr) {
-    return Math.floor(Math.random() * arr.length);
+  var getRandomIndex = function (array) {
+    return Math.ceil(Math.random() * array.length);
   };
 
-  // функция генерации объекта
-  var createPictureObject = function () {
-    var picture = {};
-    var randomIndex = getRandomIndex(srcImgs);
-    picture.url = 'photos/' + srcImgs[randomIndex] + '.jpg';
-    srcImgs.splice(randomIndex, 1);
-    picture.descriptions = imageDescriptions[getRandomIndex(imageDescriptions)];
-    picture.likes = getRandomNumber(minLikes, maxLikes);
-    picture.comments = [
+  var createPosts = function (amount) {
+    var posts = [];
+
+    for (var i = 1; i <= amount; i++) {
+      posts.push(createPost(i));
+    }
+    return posts;
+  };
+
+  var createPost = function (index) {
+    var postPhoto = 'photos/' + index + '.jpg';
+    var postDescription = getRandomElement(PHOTO_DESCRIPTIONS);
+    var postLike = getRandomNumber(PHOTO_MIN_LIKE, PHOTO_MAX_LIKE);
+    var postComments = [
       {
-        avatar: 'img/avatar-' + getRandomNumber(minCommentsAvatar, maxCommentsAvatar) + '.svg',
-        message: commentsMessages[getRandomIndex(commentsMessages)],
-        name: commentsNames[getRandomIndex(commentsNames)]
+        avatar: 'img/avatar-' + getRandomNumber(COMMENT_MIN_AVATAR_URL, COMMENT_MAX_AVATAR_URL) + '.svg',
+        message: getRandomElement(COMMENT_NAMES),
+        name: getRandomElement(COMMENT_MESSAGES)
       },
       {
-        avatar: 'img/avatar-' + getRandomNumber(minCommentsAvatar, maxCommentsAvatar) + '.svg',
-        message: commentsMessages[getRandomIndex(commentsMessages)],
-        name: commentsNames[getRandomIndex(commentsNames)]
+        avatar: 'img/avatar-' + getRandomNumber(COMMENT_MIN_AVATAR_URL, COMMENT_MAX_AVATAR_URL) + '.svg',
+        message: getRandomElement(COMMENT_NAMES),
+        name: getRandomElement(COMMENT_MESSAGES)
+      },
+      {
+        avatar: 'img/avatar-' + getRandomNumber(COMMENT_MIN_AVATAR_URL, COMMENT_MAX_AVATAR_URL) + '.svg',
+        message: getRandomElement(COMMENT_NAMES),
+        name: getRandomElement(COMMENT_MESSAGES)
       }
     ];
-    return picture;
+    return {
+      url: postPhoto,
+      descriptions: postDescription,
+      likes: postLike,
+      comments: postComments
+    };
   };
 
-  // функция создания общего массива объектов
-  var createPictures = function (count) {
-    var pictures = [];
-    for (var i = 0; i < count; i++) {
-      pictures.push(createPictureObject());
-    }
-    return pictures;
+  var createPostElement = function (post) {
+    var postElement = templatePost.cloneNode(true);
+    var postImg = postElement.querySelector('.picture__img');
+    var postLikes = postElement.querySelector('.picture__likes');
+    var postComments = postElement.querySelector('.picture__comments');
+    postImg.src = post.url;
+    postLikes.textContent = post.likes;
+    postComments.textContent = getRandomIndex(post.comments);
+    return postElement;
   };
 
-  // получаем фрагмент
-  var pictureTemplate = document.querySelector('#picture')
-    .content
-    .querySelector('.picture');
-
-  // получаем элемент для вставки фрагмента
-  var similarListPictures = document.querySelector('.pictures');
-
-  // функция формирования элемента картинки
-  var renderPicture = function (picture) {
-    var pictureElement = pictureTemplate.cloneNode(true);
-    var pictureImg = pictureElement.querySelector('.picture__img');
-    var pictureLikes = pictureElement.querySelector('.picture__likes');
-    var pictureComments = pictureElement.querySelector('.picture__comments');
-    pictureImg.src = picture.url;
-    pictureLikes.textContent = picture.likes;
-    pictureComments.textContent = picture.comments.length;
-    return pictureElement;
-  };
-
-  // функция-цикл вставки всех картинок
-  var renderPictures = function (pictures) {
+  var renderPosts = function (posts) {
     var fragment = document.createDocumentFragment();
 
-    for (var i = 0; i < pictures.length; i++) {
-      fragment.appendChild(renderPicture(pictures[i]));
+    for (var i = 0; i < posts.length; i++) {
+      fragment.appendChild(createPostElement(posts[i]));
     }
-    similarListPictures.appendChild(fragment);
+    similarListPosts.appendChild(fragment);
   };
 
-  var pictures = createPictures(countCreateImageElements);
-  renderPictures(pictures);
-
+  renderPosts(createPosts(AMOUNT_PHOTOS));
 })();
