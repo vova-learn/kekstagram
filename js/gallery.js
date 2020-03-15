fbf7de78b9f8b37f37ebdc135fd68e7e9340ccf1
Vladimir <shopme13@yandex.ru>
2020-03-15 14:23:27 +0300

    Добавил фильтрацию постов.

--- js/gallery.js
+++ js/gallery.js
@@ -4,11 +4,22 @@
   // export window.gallery: getDataPosts();
   // import window.backend: load(callback);
 
+  var MAX_POSTS = 25;
+  var RANDOM_POSTS = 10;
+
   var dataPosts;
+  var activeFilter = 'img-filters__button--active';
+  var inactiveFilter = 'img-filters--inactive';
+
   var similarListPosts = document.querySelector('.pictures');
   var templatePost = document.querySelector('#picture')
     .content
     .querySelector('.picture');
+  var filterBlock = document.querySelector('.img-filters');
+  var filterButtons = filterBlock.querySelectorAll('.img-filters__button');
+  var defaultButton = filterBlock.querySelector('#filter-default');
+  var randomButton = filterBlock.querySelector('#filter-random');
+  var discussedButton = filterBlock.querySelector('#filter-discussed');
 
   var createPostElement = function (post) {
     var postElement = templatePost.cloneNode(true);
@@ -22,17 +33,79 @@
     return postElement;
   };
 
+  var getRandomIndexPosts = function (posts) {
+    return Math.floor(Math.random() * posts.length);
+  };
+
+  var clearPosts = function (evt) {
+    var pictures = document.querySelectorAll('.picture');
+    pictures.forEach(function (picture) {
+      picture.remove();
+    });
+
+    if (!evt.target.classList.contains(activeFilter)) {
+      filterButtons.forEach(function (button) {
+        button.classList.remove(activeFilter);
+      });
+      evt.target.classList.add(activeFilter);
+    }
+  };
+
   var renderPosts = function (posts) {
     var fragment = document.createDocumentFragment();
 
-    for (var i = 0; i < posts.length; i++) {
+    for (var i = 0; i < MAX_POSTS; i++) {
       fragment.appendChild(createPostElement(posts[i]));
     }
     similarListPosts.appendChild(fragment);
   };
 
+  var renderRandomPosts = function (posts) {
+    var fragment = document.createDocumentFragment();
+    var copyPosts = posts.slice();
+
+    for (var i = 0; i < RANDOM_POSTS; i++) {
+      var indexPost = getRandomIndexPosts(copyPosts);
+      fragment.appendChild(createPostElement(copyPosts[indexPost]));
+      copyPosts.splice(indexPost, 1);
+    }
+    similarListPosts.appendChild(fragment);
+  };
+
+  var renderDiscussedPosts = function (posts) {
+    var fragment = document.createDocumentFragment();
+    var copyPosts = posts.slice();
+
+    copyPosts.sort(function (activePost, nextPost) {
+      return nextPost.comments.length - activePost.comments.length;
+    });
+
+    for (var i = 0; i < MAX_POSTS; i++) {
+      fragment.appendChild(createPostElement(copyPosts[i]));
+    }
+    similarListPosts.appendChild(fragment);
+  };
+
+  filterBlock.addEventListener('click', function (evt) {
+    if (evt.target === randomButton) {
+      debouncePosts(renderRandomPosts, evt);
+    } else if (evt.target === discussedButton) {
+      debouncePosts(renderDiscussedPosts, evt);
+    } else if (evt.target === defaultButton) {
+      debouncePosts(renderPosts, evt);
+    }
+  });
+
+  var debouncePosts = window.utility.debounce(function (cb, evt) {
+    clearPosts(evt);
+    cb(dataPosts);
+  });
+
   var getServerData = function (data) {
-    renderPosts(data);
+    if (data) {
+      renderPosts(data);
+      filterBlock.classList.remove(inactiveFilter);
+    }
     dataPosts = data;
   };
 
--- js/utility.js
+++ js/utility.js
@@ -5,12 +5,28 @@
 
   var ESC_KEY = 27;
   var ENTER_KEY = 13;
+  var DEBOUNCE_INTERVAL = 150;
   var bodyTag = document.querySelector('body');
+
+  var debounce = function (cb) {
+    var lastTimeout = null;
+
+    return function () {
+      var parameters = arguments;
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = window.setTimeout(function () {
+        cb.apply(null, parameters);
+      }, DEBOUNCE_INTERVAL);
+    };
+  };
 
   window.utility = {
     ESCAPE: ESC_KEY,
     ENTER: ENTER_KEY,
-    bodyTag: bodyTag
+    bodyTag: bodyTag,
+    debounce: debounce
   };
 
 })();
